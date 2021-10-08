bc258a02509685c38eae7ea1275b4c991a062e4f
sccala <sccala18@gmail.com>
2021-10-08 11:32:15 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {useState} from "react";
+import "./styles.css";
+import { InputTodo } from './components/InputTodo';
+import { IncompleteTodos } from './components/IncompleteTodos';
+import { CompleteTodos } from './components/CompleteTodos';
 
-function App() {
+
+export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompeleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompeleteTodos, todoText]
+    setIncompleteTodos(newTodos)
+    setTodoText("")
+  }
+  const onClickDelete = (index) => {
+    const newTodos = [...incompeleteTodos];
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos)
+  }
+
+  const onClickComplete = (index) => {
+    const newIncompleteTodos = [...incompeleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completeTodos, incompeleteTodos[index]];
+    setIncompleteTodos(newIncompleteTodos);
+    setCompleteTodos(newCompleteTodos);
+  }
+
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompeleteTodos, completeTodos[index]]
+    setCompleteTodos(newCompleteTodos)
+    setIncompleteTodos(newIncompleteTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+        disabled={incompeleteTodos.length >= 5}
+      />
+      {incompeleteTodos.length >= 5 && (<p　style={{color:'red'}}>登録できるtodoは５つまでです。消化してください。</p>) }
+      
+      <IncompleteTodos
+        todos={incompeleteTodos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos 
+      todos ={completeTodos}
+        onClickBack={onClickBack}
+        />
+    </>
+
   );
 }
-
-export default App;
